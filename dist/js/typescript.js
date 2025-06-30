4a0eef83c3100bac8840fc4fde4c0a3fa1156c79
AislanJTelesF <aislanjesus2019@gmail.com>
2025-06-30 09:30:25 -0300

    final etapa 3

--- dist/js/typescript.js
+++ dist/js/typescript.js
@@ -1,24 +1,29 @@
-// Tipos Primitivos
-let valor = 3000;
-let nome = "";
-let isPago = false;
-let qualquer = "";
-qualquer = 22;
-// Arrays
-const lista = [];
-lista.push("Jhonatan", "Cachorro", 22, true, []);
-const lista2 = [];
-lista2.push(12, 45, 22, 0, 30);
-// Enum
-var TipoTransacao;
-(function (TipoTransacao) {
-    TipoTransacao["DEPOSITO"] = "Dep\u00F3sito";
-    TipoTransacao["TRANSFERENCIA"] = "Transfer\u00EAncia";
-    TipoTransacao["PAGAMENTO_BOLETO"] = "Pagamento de Boleto";
-})(TipoTransacao || (TipoTransacao = {}));
-const novaTransacao = {
-    tipoTransacao: TipoTransacao.DEPOSITO,
-    data: new Date(),
-    valor: 0,
-};
-//
+// // Tipos Primitivos
+// let valor: number = 3000;
+// let nome: string = "";
+// let isPago: boolean = false;
+// let qualquer: any = "";
+// qualquer = 22;
+// // Arrays
+// const lista = [];
+// lista.push("Jhonatan", "Cachorro", 22, true, []);
+// const lista2: number[] = [];
+// lista2.push(12, 45, 22, 0, 30);
+// // Tipos Personalizados (Type Alias)
+// type Transacao = {
+//   tipoTransacao: TipoTransacao;
+//   data: Date;
+//   valor: number;
+// };
+// // Enum
+// enum TipoTransacao {
+//   DEPOSITO = "Depósito",
+//   TRANSFERENCIA = "Transferência",
+//   PAGAMENTO_BOLETO = "Pagamento de Boleto",
+// }
+// const novaTransacao: Transacao = {
+//   tipoTransacao: TipoTransacao.DEPOSITO,
+//   data: new Date(),
+//   valor: 0,
+// };
+// console.log(novaTransacao);
--- src/typescript.ts
+++ src/typescript.ts
@@ -1,38 +1,38 @@
-// Tipos Primitivos
+// // Tipos Primitivos
 
-let valor: number = 3000;
-let nome: string = "";
-let isPago: boolean = false;
-let qualquer: any = "";
-qualquer = 22;
+// let valor: number = 3000;
+// let nome: string = "";
+// let isPago: boolean = false;
+// let qualquer: any = "";
+// qualquer = 22;
 
-// Arrays
+// // Arrays
 
-const lista = [];
-lista.push("Jhonatan", "Cachorro", 22, true, []);
+// const lista = [];
+// lista.push("Jhonatan", "Cachorro", 22, true, []);
 
-const lista2: number[] = [];
-lista2.push(12, 45, 22, 0, 30);
+// const lista2: number[] = [];
+// lista2.push(12, 45, 22, 0, 30);
 
-// Tipos Personalizados (Type Alias)
+// // Tipos Personalizados (Type Alias)
 
-type Transacao = {
-  tipoTransacao: TipoTransacao;
-  data: Date;
-  valor: number;
-};
+// type Transacao = {
+//   tipoTransacao: TipoTransacao;
+//   data: Date;
+//   valor: number;
+// };
 
-// Enum
-enum TipoTransacao {
-  DEPOSITO = "Depósito",
-  TRANSFERENCIA = "Transferência",
-  PAGAMENTO_BOLETO = "Pagamento de Boleto",
-}
+// // Enum
+// enum TipoTransacao {
+//   DEPOSITO = "Depósito",
+//   TRANSFERENCIA = "Transferência",
+//   PAGAMENTO_BOLETO = "Pagamento de Boleto",
+// }
 
-const novaTransacao: Transacao = {
-  tipoTransacao: TipoTransacao.DEPOSITO,
-  data: new Date(),
-  valor: 0,
-};
+// const novaTransacao: Transacao = {
+//   tipoTransacao: TipoTransacao.DEPOSITO,
+//   data: new Date(),
+//   valor: 0,
+// };
 
-//
+// console.log(novaTransacao);
